94bc5d694f6cddd86a0dd12755137881cd837b3a
Kevin Crooks <kevin.crooks@summatic.co.uk>
2023-09-21 12:54:13 +0100

    Initial commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿using Foundation;
+using UIKit;
+using System;
+namespace Summatic.iOS
+{
+
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register(nameof(AppDelegate))]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        private readonly UIWindow window = new UIWindow(UIScreen.MainScreen.Bounds);
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            LogToDevice("Starting FinishedLaunching");
+            try
+            {
+                window.RootViewController = new UIViewController();
+                window.MakeKeyAndVisible();
+
+                LogToDevice("Ending FinishedLaunching");
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                LogToDevice($"EXCEPTION {ex.Message} ST:{ex.StackTrace}");
+                return true;
+            }
+        }
+
+        public static void LogToDevice(string message)
+        {
+            Console.WriteLine($"ѦSUMMATIC CWL: {message}");
+        }
+    }
+}
---
+++ Platforms/iOS/Main.cs
@@ -0,0 +1,20 @@
+﻿using UIKit;
+using System.Globalization;
+
+namespace Summatic.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.CurrentUICulture = CultureInfo.InvariantCulture;
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            AppDelegate.LogToDevice("Starting MAIN");
+            UIApplication.Main(args, null, typeof(AppDelegate));
+            AppDelegate.LogToDevice("Ending MAIN");
+        }
+    }
+}
